9417e2b8b2a9c3b3323d6b647f6961eebf63383b
Acer <deepadey20may@gmail.com>
2022-08-13 12:58:30 -0700

    this is my 2nd code push in git

--- mongodbtest1.py
+++ mongodbtest1.py
@@ -12,4 +12,13 @@ db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d)
 
-
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
